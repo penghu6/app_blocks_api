21c06c796d207bd942032a65622f9545adde1380
penghu <qq201105831@gmail.com>
2024-10-15 22:03:36 +0800

    版本管理

--- src/application/services/version_management_service.rs
+++ src/application/services/version_management_service.rs
@@ -2,16 +2,14 @@ use crate::domain::model::version_management_model::VersionManagementModel;
 use crate::domain::repositories::version_management_repository::VersionManagementRepository;
 use sea_orm::DbErr;
 use std::sync::Arc;
+use di::injectable;
 
-pub struct VersionManagementService<R: VersionManagementRepository> {
-    repository: Arc<R>,
+#[injectable]
+pub struct VersionManagementService {
+    repository: Arc<dyn VersionManagementRepository>,
 }
 
-impl<R: VersionManagementRepository> VersionManagementService<R> {
-    pub fn new(repository: Arc<R>) -> Self {
-        Self { repository }
-    }
-
+impl VersionManagementService {
     pub async fn create(&self, model: VersionManagementModel) -> Result<VersionManagementModel, DbErr> {
         self.repository.create(model).await
     }
--- src/config/inject_config.rs
+++ src/config/inject_config.rs
@@ -3,8 +3,10 @@ use std::sync::Arc;
 use di::{existing_as_self, singleton, Injectable, Ref, ServiceCollection, ServiceProvider};
 use lazy_static::lazy_static;
 use crate::application::services::user_component_service::UserComponentService;
+use crate::application::services::version_management_service::VersionManagementService;
 use crate::infrastructure::persistence::user_component_repository_impl::UserComponentRepositoryImpl;
 use crate::config::DatabaseManager;
+use crate::infrastructure::persistence::version_management_repository_impl::VersionManagementRepositoryImpl;
 
 lazy_static! {
     pub static ref DI: ServiceProvider = {
@@ -12,6 +14,8 @@ lazy_static! {
             .add(existing_as_self(DatabaseManager::new()))
             .add(UserComponentService::singleton())
             .add(UserComponentRepositoryImpl::singleton())
+            .add(VersionManagementService::singleton())
+            .add(VersionManagementRepositoryImpl::singleton())
             .build_provider()
             .expect("Failed to build DI provider.");
 
